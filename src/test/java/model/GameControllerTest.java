b13cfe3f49411245bb429a60d5d451e252eae585
Luke Wejman <lukewejman2027@u.northwestern.edu>
2026-05-03 12:08:18 -0500

    controller_test_class_created

---
+++ src/test/java/model/GameControllerTest.java
@@ -0,0 +1,25 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.easymock.EasyMock;
+import org.junit.jupiter.api.Test;
+
+public class GameControllerTest {
+
+// shouldInitializeTurnOrderAfterSetup()
+
+// shouldSetCurrentPlayerToFirstPlayerInTurnOrder()
+
+// shouldSetGamePhaseToReinforcementWhenSetupCompletes()
+
+// shouldReturnFullyInitializedGameState()
+
+
+}
+
