a97b5c74b4a0f0d944401ae8aeb7a1fd7a0ec218
doc4science <doc4science@gmail.com>
2026-05-04 02:52:15 -0500

    begin BVA for validatePlayerCount

---
+++ src/main/java/service/GameSetupService.java
@@ -0,0 +1,16 @@
+package service;
+
+import model.Player;
+
+import java.util.List;
+
+public class PlayerSetupService {
+
+    public List<Player> createPlayers(List<String> names, List<PlayerColor> colors) {
+        return null;
+    }
+
+    public void validateUniqueColors(List<PlayerColor> colors) {
+    }
+
+}
