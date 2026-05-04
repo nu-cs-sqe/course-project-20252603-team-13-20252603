43e0012226dd590d1ed732d0ea53315e6f751c8e
Luke Wejman <lukewejman2027@u.northwestern.edu>
2026-05-03 20:36:37 -0500

    gameStateCreation-6-players-tested

--- src/test/java/model/GameControllerTest.java
+++ src/test/java/model/GameControllerTest.java
@@ -38,8 +38,23 @@ void createGameStateFunctional_3_Players(){
     assertEquals(21, gameState1.getPlayers().get(0).getRemainingArmiesToPlace());
     assertEquals(21, gameState1.getPlayers().get(1).getRemainingArmiesToPlace());
     assertEquals(21, gameState1.getPlayers().get(2).getRemainingArmiesToPlace());
+}
 
+@Test
+void createGameStateFunctional_6_Players(){
+    List<String> names = List.of("Adam", "Bob", "Chloe", "David", "Earl", "Faith");
+    List<String> colors = List.of("Red", "Yellow", "Blue", "Green", "Orange", "Purple");
+    GameController gameController1 = new GameController();
+    GameState gameState1 = gameController1.createNewGame(names, colors);
+    assertEquals(42, gameState1.getTerritories().size());
+    assertEquals(6, gameState1.getPlayers().size());
 
+    assertEquals(13, gameState1.getPlayers().get(0).getRemainingArmiesToPlace());
+    assertEquals(13, gameState1.getPlayers().get(1).getRemainingArmiesToPlace());
+    assertEquals(13, gameState1.getPlayers().get(2).getRemainingArmiesToPlace());
+    assertEquals(13, gameState1.getPlayers().get(3).getRemainingArmiesToPlace());
+    assertEquals(13, gameState1.getPlayers().get(4).getRemainingArmiesToPlace());
+    assertEquals(13, gameState1.getPlayers().get(5).getRemainingArmiesToPlace());
 }
 
 // shouldInitializeTurnOrderAfterSetup()
