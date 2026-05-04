e2832066c5e2b66a7795b68f76174f533c2b777f
Luke Wejman <lukewejman2027@u.northwestern.edu>
2026-05-04 18:08:59 -0500

    fixed format to align to other teammates file storage and work
    # Conflicts:
    #	src/main/java/service/GameSetupService.java
    #	src/test/java/service/GameSetupServiceTest.java

--- src/main/java/service/GameSetupService.java
+++ src/main/java/service/GameSetupService.java
@@ -1,16 +1,13 @@
 package service;
 
+import model.GamePhase;
+import model.GameState;
 import model.Player;
 
 import java.util.List;
 
-public class PlayerSetupService {
+public class GameSetupService {
 
-    public List<Player> createPlayers(List<String> names, List<PlayerColor> colors) {
-        return null;
-    }
-
-    public void validateUniqueColors(List<PlayerColor> colors) {
     public static final int MIN_PLAYERS = 2;
     public static final int MAX_PLAYERS = 6;
 
@@ -22,4 +19,36 @@ public class PlayerSetupService {
         }
     }
 
-}
+    public void initializeTurnOrder(GameState gameState){
+        List<Player> players_TO = gameState.getPlayers();
+        // Collections.shuffle(players_TO);
+        gameState.setTurnOrder(players_TO);
+    }
+
+    public void startFirstTurn(GameState gameState){
+        List<Player> players_TO = gameState.getTurnOrder();
+        Player first_pl = players_TO.get(0);
+        gameState.setCurrentPlayer(first_pl);
+    }
+    public GameState createNewGame(List<String> names, List<String> colors){
+        // create a new game state
+        GameState gameState = new GameState();
+        // set the phase to setup
+        // call player generation
+        // call territory generation
+        // call territory distribution
+
+        return gameState;
+    }
+    public GameState orchestration(List<String> pre_names, List<String> pre_colors){
+        GameState gameState = createNewGame(pre_names, pre_colors);
+        initializeTurnOrder(gameState);
+        startFirstTurn(gameState);
+        gameState.setCurrentPhase(GamePhase.REINFORCEMENT);
+        return gameState;
+
+    }
+    }
+
+
+
