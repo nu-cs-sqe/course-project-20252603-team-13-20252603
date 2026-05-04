a680e672218a879d741162d619528a2fce3b5add
Luke Wejman <lukewejman2027@u.northwestern.edu>
2026-05-04 18:08:59 -0500

    fixed format to align to other teammates file storage and work
    # Conflicts:
    #	src/main/java/service/GameSetupService.java
    #	src/test/java/service/GameSetupServiceTest.java

--- src/test/java/service/GameSetupServiceTest.java
+++ src/test/java/service/GameSetupServiceTest.java
@@ -1,62 +1,66 @@
 package service;
 
+import model.Player;
+import model.Territory;
+import model.GameState;
+import model.GamePhase;
+
+
+
+import java.util.List;
+
+import org.easymock.EasyMock;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GameSetupServiceTest {
 
-    @Test
-    void TC1_ValidateMinimumSizeGame() {
-        GameSetupService service = new GameSetupService();
-        int playerCount = 2;
-        assertDoesNotThrow(() -> service.validatePlayerCount(playerCount));
-    }
+// shouldInitializeTurnOrderAfterSetup()
+@Test
+void shouldInitializeTurnOrderAfterSetup() {
+    List<String> names = List.of("Adam", "Bob");
+    List<String> colors = List.of("Red", "Yellow");
+    GameSetupService gameSetupService = new GameSetupService();
+    GameState gameState1 = gameSetupService.orchestration(names, colors);
 
-    @Test
-    void TC2_ValidateBelowMinimumSizeGame() {
-        GameSetupService service = new GameSetupService();
-        int playerCount = 1;
-        assertThrows(IllegalArgumentException.class,
-                () -> service.validatePlayerCount(playerCount));
-    }
+    assertNotNull(gameState1.getTurnOrder());
+}
 
-    @Test
-    void TC3_ValidateMaximumSizeGame() {
-        GameSetupService service = new GameSetupService();
-        int playerCount = 6;
-        assertDoesNotThrow(() -> service.validatePlayerCount(playerCount));
-    }
+@Test
+void shouldSetCurrentPlayerToFirstPlayerInTurnOrder() {
+    List<String> names = List.of("Adam", "Bob");
+    List<String> colors = List.of("Red", "Yellow");
+    GameSetupService gameSetupService = new GameSetupService();
+    GameState gameState1 = gameSetupService.orchestration(names, colors);
 
-    @Test
-    void TC4_ValidateAboveMaximumSizeGame() {
-        GameSetupService service = new GameSetupService();
-        int playerCount = 7;
-        assertThrows(IllegalArgumentException.class,
-                () -> service.validatePlayerCount(playerCount));
-    }
+    Player player1_actual = gameState1.getCurrentPlayer();
+    Player player1_expected = gameState1.getPlayers().get(0);
 
-    @Test
-    void TC5_ValidateINT_MAX() {
-        GameSetupService service = new GameSetupService();
-        int playerCount = Integer.MAX_VALUE;
-        assertThrows(IllegalArgumentException.class,
-                () -> service.validatePlayerCount(playerCount));
-    }
+    assertEquals(player1_expected, player1_actual);
+    assertSame(player1_expected, player1_actual);
+}
 
-    @Test
-    void TC6_ValidateINT_MIN() {
-        GameSetupService service = new GameSetupService();
-        int playerCount = Integer.MIN_VALUE;
-        assertThrows(IllegalArgumentException.class,
-                () -> service.validatePlayerCount(playerCount));
-    }
+@Test
+void shouldSetGamePhaseToReinforcementWhenSetupCompletes(){
+    List<String> names = List.of("Adam", "Bob");
+    List<String> colors = List.of("Red", "Yellow");
+    GameSetupService gameSetupService = new GameSetupService();
+    GameState gameState1 = gameSetupService.orchestration(names, colors);
+    assertEquals(GamePhase.REINFORCEMENT, gameState1.getCurrentPhase());
+}
 
-    @Test
-    void TC7_ValidateInRangeOfValidGame() {
-        GameSetupService service = new GameSetupService();
-        int playerCount = 4;
-        assertDoesNotThrow(() -> service.validatePlayerCount(playerCount));
-    }
+@Test
+void shouldReturnFullyInitializedGameState() {
+    List<String> names = List.of("Adam", "Bob");
+    List<String> colors = List.of("Red", "Yellow");
+    GameSetupService gameSetupService = new GameSetupService();
+    GameState gameState1 = gameSetupService.orchestration(names, colors);
+    assertEquals(names.size(), gameState1.getPlayers().size());
+    assertEquals(42, gameState1.getTerritories().size());
 
 }
+
+
+}
+
