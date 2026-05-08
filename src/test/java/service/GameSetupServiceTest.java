f082459f031032cb93973fd566dc978cca28f255
doc4science <doc4science@gmail.com>
2026-05-08 05:12:56 -0500

    TC1_ValidateMinimumSizeGame Validated OK

--- src/test/java/service/GameSetupServiceTest.java
+++ src/test/java/service/GameSetupServiceTest.java
@@ -1,4 +1,14 @@
 package service;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class GameSetupServiceTest {
+
+    @Test
+    void TC1_ValidateMinimumSizeGame() {
+        GameSetupService service = new GameSetupService();
+        assertTrue(service.validatePlayerCount(2));
+    }
 }
