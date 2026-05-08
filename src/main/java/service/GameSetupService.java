03da904699814117d1e941c9fb0f5a0a1ed00500
doc4science <doc4science@gmail.com>
2026-05-08 05:31:06 -0500

    change from boolean to void method per design doc, refactor all tests based on void function, all tests pass OK

--- src/main/java/service/GameSetupService.java
+++ src/main/java/service/GameSetupService.java
@@ -9,8 +9,12 @@ public class GameSetupService {
     public static final int MIN_PLAYERS = 2;
     public static final int MAX_PLAYERS = 6;
 
-    public boolean validatePlayerCount(int playerCount) {
-        return playerCount >= MIN_PLAYERS && playerCount <= MAX_PLAYERS;
+    public void validatePlayerCount(int playerCount) {
+        if (playerCount < MIN_PLAYERS || playerCount > MAX_PLAYERS){
+            throw new IllegalArgumentException(
+                    "Player count must be between " + MIN_PLAYERS + " and " + MAX_PLAYERS + ", but got: " + playerCount
+            );
+        }
     }
 
 }
