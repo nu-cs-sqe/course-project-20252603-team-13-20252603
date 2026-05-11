32b8fe8092077a88f4c3f098e245470c735b2b50
Luke Wejman <lukewejman2027@u.northwestern.edu>
2026-05-10 19:24:37 -0500

    initial file creation

---
+++ src/main/java/service/ReinforcementService.java
@@ -0,0 +1,18 @@
+package service;
+
+import model.GamePhase;
+import model.GameState;
+import model.Player;
+
+import java.util.List;
+
+public class ReinforcementService {
+
+
+    int calculateBaseReinforcements(Player player, GameState gameState) {
+
+    }
+    //Optional helper:
+    //int countTerritoriesOwnedBy(Player player, GameState gameState)
+
+}
---
+++ src/test/java/service/ReinforcementServiceTest.java
@@ -0,0 +1,13 @@
+package service;
+
+import model.GamePhase;
+import model.GameState;
+import model.Player;
+
+import java.util.List;
+
+public class ReinforcementServiceTest {
+
+
+
+}
