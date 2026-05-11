60c73572629d252925586bdad58a0c9aeedaa051
Luke Wejman <lukewejman2027@u.northwestern.edu>
2026-05-11 13:01:13 -0500

    shouldReturnMinimumReinforcementWhenPlayerOwnsFewTerritories passing, calculation changed, removal of helper function

--- src/main/java/service/ReinforcementService.java
+++ src/main/java/service/ReinforcementService.java
@@ -11,22 +11,11 @@ public class ReinforcementService {
 
 
     int calculateBaseReinforcements(Player player, GameState gameState) {
-        int owner_territories = countTerritoriesOwnedBy(player, gameState);
+        int owner_territories = player.getControlledTerritoryCount();
         int base_reinforcements = Math.max(3, owner_territories / 3);
         return base_reinforcements;
     }
     //Optional helper:
-    int countTerritoriesOwnedBy(Player player, GameState gameState) {
-        List<Territory> territories = gameState.getTerritories();
-        int count = 0;
-        for (int i = 0; i < territories.size(); i++){
-            Territory current = territories.get(i);
-            Player curr_player = current.getOwner();
-            if (curr_player == player) {
-                count++;
-            }
-        }
-        return count;
-    }
+
 
 }
--- src/test/java/service/ReinforcementServiceTest.java
+++ src/test/java/service/ReinforcementServiceTest.java
@@ -3,9 +3,12 @@ package service;
 import model.GamePhase;
 import model.GameState;
 import model.Player;
+import model.Continent;
+import service.ReinforcementService;
 
 import java.util.List;
 
+import model.Territory;
 import org.easymock.EasyMock;
 import org.junit.jupiter.api.Test;
 
@@ -15,7 +18,19 @@ public class ReinforcementServiceTest {
 
     @Test
     void shouldReturnMinimumReinforcementWhenPlayerOwnsFewTerritories() {
-
+        ReinforcementService rs = new ReinforcementService();
+        List<Territory> empty = List.of();
+        Player fake_player = new Player(0, "fake", "fake", 0, empty);
+        Territory t1 = new Territory();
+                //("Alaska", fake_player, 2, Continent.NORTH_AMERICA);
+        Territory t2 = new Territory();
+        //("Greenland", fake_player, 2, Continent.NORTH_AMERICA);
+        List<Territory> controlled_territories = List.of(t1, t2);
+        GameState gameState1 = new GameState();
+        Player player1 = new Player(1, "A", "Red", 0, controlled_territories);
+        gameState1.setTerritories(controlled_territories);
+        int actual_owned_terr = rs.calculateBaseReinforcements(player1, gameState1);
+        assertEquals(3, actual_owned_terr);
     }
 
     @Test
