3da1e23d980db25365c72d2d5475bbc5b6765e0d
Luke Wejman <lukewejman2027@u.northwestern.edu>
2026-05-10 21:10:19 -0500

    initial function verisons completed

--- src/main/java/service/ReinforcementService.java
+++ src/main/java/service/ReinforcementService.java
@@ -4,13 +4,27 @@ import model.Player;
 import model.GameState;
 import model.Territory;
 
+import java.util.List;
+
 public class ReinforcementService {
 
 
     void placeReinforcements(Player player, Territory territory, int armies, GameState gameState){
         boolean result = canPlaceReinforcements(player, territory, armies, gameState);
         if (result) {
-            
+            List<Player> players = gameState.getPlayers();
+            int player_index = players.indexOf(player);
+            Player pl_found = gameState.getPlayers().get(player_index);
+            List<Territory> territories = gameState.getTerritories();
+            int territory_index = territories.indexOf(territory);
+            Territory tr_found = gameState.getTerritories().get(territory_index);
+
+            int current_armies_unplaced = pl_found.getRemainingArmiesToPlace();
+            int new_remaining_armies = current_armies_unplaced - armies;
+            int current_territories_placed = tr_found.getArmyCount();
+            int new_territory_army = armies + current_territories_placed;
+            tr_found.setArmyCount(new_territory_army);
+            pl_found.setRemainingArmiesToPlace(new_remaining_armies);
         }
         else {
             throw new IllegalArgumentException("Your army value or territory is not valid, please try again.");
@@ -19,11 +33,17 @@ public class ReinforcementService {
     }
     //Optional helpers:
     boolean canPlaceReinforcements(Player player, Territory territory, int armies, GameState gameState){
+        List<Player> players = gameState.getPlayers();
+        int player_index = players.indexOf(player);
+        Player pl_found = gameState.getPlayers().get(player_index);
+        List<Territory> territories = gameState.getTerritories();
+        int territory_index = territories.indexOf(territory);
+        Territory tr_found = gameState.getTerritories().get(territory_index);
 
-        if (player != territory.getOwner()){
+        if (pl_found != tr_found.getOwner()){
             return false;
         }
-        if (player.getRemainingArmiesToPlace() < armies) {
+        if (pl_found.getRemainingArmiesToPlace() < armies) {
             return false;
         }
         return true;
