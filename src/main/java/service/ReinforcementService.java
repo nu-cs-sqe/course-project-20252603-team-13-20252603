3d35e394d301908a5dff48b53033ce6623541b83
Luke Wejman <lukewejman2027@u.northwestern.edu>
2026-05-10 20:10:45 -0500

    initial skeleton function code + file creation

---
+++ src/main/java/service/ReinforcementService.java
@@ -0,0 +1,24 @@
+package service;
+
+
+import model.GameState;
+import model.Player;
+import model.Territory;
+
+import java.util.List;
+
+public class ReinforcementService {
+
+
+    int calculateContinentBonus(Player player, GameState gameState){
+
+    }
+    //Optional helpers:
+    boolean controlsContinent(Player player, Continent continent, GameState gameState){
+
+    }
+    List<Territory> getTerritoriesInContinent(Continent continent){
+
+    }
+
+}
