bc1f61fb0687276b4e45551d703630d1f55ad6c7
Luke Wejman <lukewejman2027@u.northwestern.edu>
2026-05-11 08:07:15 -0500

    BVA file + skeleton tests

--- src/test/java/service/ReinforcementServiceTest.java
+++ src/test/java/service/ReinforcementServiceTest.java
@@ -1,4 +1,44 @@
 package service;
 
+import model.Player;
+import model.Territory;
+import model.GameState;
+import model.GamePhase;
+
+
+
+import java.util.List;
+
+import org.easymock.EasyMock;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ReinforcementServiceTest {
+
+    @Test
+    void shouldAllowPlayerToPlaceReinforcementsOnOwnedTerritory() {
+
+    }
+
+    @Test
+    void shouldRejectPlacementOnTerritoryNotOwnedByPlayer() {
+
+    }
+
+    @Test
+    void shouldRejectPlacementWhenArmiesExceedRemainingReinforcements() {
+        
+    }
+
+    @Test
+    void shouldDecreaseRemainingReinforcementsAfterValidPlacement() {
+
+    }
+
+    @Test
+    void shouldUpdateTerritoryArmyCountAfterValidPlacement() {
+
+    }
+
 }
