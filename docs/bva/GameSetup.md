b2d075d4bbbf2a7f968d6f0f101ff7b0b78863e8
Luke Wejman <lukewejman2027@u.northwestern.edu>
2026-05-11 06:48:41 -0500

    BVA and Test cases are updated

--- docs/bva/GameSetup.md
+++ docs/bva/GameSetup.md
@@ -24,13 +24,17 @@
 # Game Setup BVA
 
 ### Method under test: validatePlayerCount
-|                                  | State of the System    | Expected output             | Implemented?             |
-|----------------------------------|------------------------|-----------------------------|--------------------------|
-| TC1_ValidateMinimumSizeGame      | playerCount is 2       | The playerCount is accepted | :white_check_mark:        |
-| TC2_ValidateBelowMinimumSizeGame | playerCount is 1       | The playerCount is rejected | :white_check_mark:        |
-| TC3_ValidateMaximumSizeGame      | playerCount is 6       | The playerCount is accepted | :white_check_mark:        |
-| TC4_ValidateAboveMaximumSizeGame | playerCount is 7       | The playerCount is rejected | :white_check_mark:        |
-| TC5_ValidateINT_MAX              | playerCount is INT_MAX | The playerCount is rejected | :white_check_mark:        |
-| TC6_ValidateINT_MIN              | playerCount is INT_MIN | The playerCount is rejected | :white_check_mark: |
-| TC7_ValidateInRangeOfValidGame   | playerCount is 4       | The playerCount is accepted | :white_check_mark: |
+|                                                          | State of the System    | Expected output                                                   | Implemented?       |
+|----------------------------------------------------------|------------------------|-------------------------------------------------------------------|--------------------|
+| TC1_ValidateMinimumSizeGame                              | playerCount is 2       | The playerCount is accepted                                       | :white_check_mark: |
+| TC2_ValidateBelowMinimumSizeGame                         | playerCount is 1       | The playerCount is rejected                                       | :white_check_mark: |
+| TC3_ValidateMaximumSizeGame                              | playerCount is 6       | The playerCount is accepted                                       | :white_check_mark: |
+| TC4_ValidateAboveMaximumSizeGame                         | playerCount is 7       | The playerCount is rejected                                       | :white_check_mark: |
+| TC5_ValidateINT_MAX                                      | playerCount is INT_MAX | The playerCount is rejected                                       | :white_check_mark: |
+| TC6_ValidateINT_MIN                                      | playerCount is INT_MIN | The playerCount is rejected                                       | :white_check_mark: |
+| TC7_ValidateInRangeOfValidGame                           | playerCount is 4       | The playerCount is accepted                                       | :white_check_mark: |
+| TC8_shouldInitializeTurnOrderAfterSetup                  | playerCount is 2       | The turnOrder is not Null                                         | :white_check_mark: |
+| TC9_shouldSetCurrentPlayerToFirstPlayerInTurnOrder       | playerCount is 2       | The firstTurn is the first player                                 | :white_check_mark: |
+| TC10_shouldSetGamePhaseToReinforcementWhenSetupCompletes | playerCount is 2       | GamePhase is REINFORCEMENT                                        | :white_check_mark: |
+| TC11_shouldReturnFullyInitializedGameState               | playerCount is 2       | Finished gameState: players, territories, turnorder, are not null | :white_check_mark: |
 
--- src/test/java/service/GameSetupServiceTest.java
+++ src/test/java/service/GameSetupServiceTest.java
@@ -16,50 +16,105 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class GameSetupServiceTest {
 
-// shouldInitializeTurnOrderAfterSetup()
-@Test
-void shouldInitializeTurnOrderAfterSetup() {
-    List<String> names = List.of("Adam", "Bob");
-    List<String> colors = List.of("Red", "Yellow");
-    GameSetupService gameSetupService = new GameSetupService();
-    GameState gameState1 = gameSetupService.orchestration(names, colors);
+    @Test
+    void TC1_ValidateMinimumSizeGame() {
+        GameSetupService service = new GameSetupService();
+        int playerCount = 2;
+        assertDoesNotThrow(() -> service.validatePlayerCount(playerCount));
+    }
 
-    assertNotNull(gameState1.getTurnOrder());
-}
+    @Test
+    void TC2_ValidateBelowMinimumSizeGame() {
+        GameSetupService service = new GameSetupService();
+        int playerCount = 1;
+        assertThrows(IllegalArgumentException.class,
+                () -> service.validatePlayerCount(playerCount));
+    }
 
-@Test
-void shouldSetCurrentPlayerToFirstPlayerInTurnOrder() {
-    List<String> names = List.of("Adam", "Bob");
-    List<String> colors = List.of("Red", "Yellow");
-    GameSetupService gameSetupService = new GameSetupService();
-    GameState gameState1 = gameSetupService.orchestration(names, colors);
+    @Test
+    void TC3_ValidateMaximumSizeGame() {
+        GameSetupService service = new GameSetupService();
+        int playerCount = 6;
+        assertDoesNotThrow(() -> service.validatePlayerCount(playerCount));
+    }
 
-    Player player1_actual = gameState1.getCurrentPlayer();
-    Player player1_expected = gameState1.getPlayers().get(0);
+    @Test
+    void TC4_ValidateAboveMaximumSizeGame() {
+        GameSetupService service = new GameSetupService();
+        int playerCount = 7;
+        assertThrows(IllegalArgumentException.class,
+                () -> service.validatePlayerCount(playerCount));
+    }
 
-    assertEquals(player1_expected, player1_actual);
-    assertSame(player1_expected, player1_actual);
-}
+    @Test
+    void TC5_ValidateINT_MAX() {
+        GameSetupService service = new GameSetupService();
+        int playerCount = Integer.MAX_VALUE;
+        assertThrows(IllegalArgumentException.class,
+                () -> service.validatePlayerCount(playerCount));
+    }
 
-@Test
-void shouldSetGamePhaseToReinforcementWhenSetupCompletes(){
-    List<String> names = List.of("Adam", "Bob");
-    List<String> colors = List.of("Red", "Yellow");
-    GameSetupService gameSetupService = new GameSetupService();
-    GameState gameState1 = gameSetupService.orchestration(names, colors);
-    assertEquals(GamePhase.REINFORCEMENT, gameState1.getCurrentPhase());
-}
+    @Test
+    void TC6_ValidateINT_MIN() {
+        GameSetupService service = new GameSetupService();
+        int playerCount = Integer.MIN_VALUE;
+        assertThrows(IllegalArgumentException.class,
+                () -> service.validatePlayerCount(playerCount));
+    }
 
-@Test
-void shouldReturnFullyInitializedGameState() {
-    List<String> names = List.of("Adam", "Bob");
-    List<String> colors = List.of("Red", "Yellow");
-    GameSetupService gameSetupService = new GameSetupService();
-    GameState gameState1 = gameSetupService.orchestration(names, colors);
-    assertEquals(names.size(), gameState1.getPlayers().size());
-    assertEquals(42, gameState1.getTerritories().size());
+    @Test
+    void TC7_ValidateInRangeOfValidGame() {
+        GameSetupService service = new GameSetupService();
+        int playerCount = 4;
+        assertDoesNotThrow(() -> service.validatePlayerCount(playerCount));
+    }
 
-}
+    // shouldInitializeTurnOrderAfterSetup()
+    @Test
+    void TC8_shouldInitializeTurnOrderAfterSetup() {
+        List<String> names = List.of("Adam", "Bob");
+        List<String> colors = List.of("Red", "Yellow");
+        GameSetupService gameSetupService = new GameSetupService();
+        GameState gameState1 = gameSetupService.orchestration(names, colors);
+
+        assertNotNull(gameState1.getTurnOrder());
+    }
+
+    @Test
+    void TC9_shouldSetCurrentPlayerToFirstPlayerInTurnOrder() {
+        List<String> names = List.of("Adam", "Bob");
+        List<String> colors = List.of("Red", "Yellow");
+        GameSetupService gameSetupService = new GameSetupService();
+        GameState gameState1 = gameSetupService.orchestration(names, colors);
+
+        Player player1_actual = gameState1.getCurrentPlayer();
+        Player player1_expected = gameState1.getPlayers().get(0);
+
+        assertEquals(player1_expected, player1_actual);
+        assertSame(player1_expected, player1_actual);
+    }
+
+    @Test
+    void TC10_shouldSetGamePhaseToReinforcementWhenSetupCompletes(){
+        List<String> names = List.of("Adam", "Bob");
+        List<String> colors = List.of("Red", "Yellow");
+        GameSetupService gameSetupService = new GameSetupService();
+        GameState gameState1 = gameSetupService.orchestration(names, colors);
+        assertEquals(GamePhase.REINFORCEMENT, gameState1.getCurrentPhase());
+    }
+
+    @Test
+    void TC11_shouldReturnFullyInitializedGameState() {
+        List<String> names = List.of("Adam", "Bob");
+        List<String> colors = List.of("Red", "Yellow");
+        GameSetupService gameSetupService = new GameSetupService();
+        GameState gameState1 = gameSetupService.orchestration(names, colors);
+        assertEquals(names.size(), gameState1.getPlayers().size());
+        assertNotNull(gameState1.getTerritories());
+        assertNotNull(gameState1.getPlayers());
+        assertEquals(42, gameState1.getTerritories().size());
+
+    }
 
 
 }
