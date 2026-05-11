1e38dbb300e695e9c452f9c14c0552c0191f86a6
Luke Wejman <lukewejman2027@u.northwestern.edu>
2026-05-10 20:40:19 -0500

    initial code added, no tested, not tested

--- src/main/java/service/ReinforcementService.java
+++ src/main/java/service/ReinforcementService.java
@@ -4,6 +4,8 @@ package service;
 import model.GameState;
 import model.Player;
 import model.Territory;
+import model.Continent;
+import domain.TerritoryCatalog;
 
 import java.util.List;
 
@@ -11,14 +13,45 @@ public class ReinforcementService {
 
 
     int calculateContinentBonus(Player player, GameState gameState){
+        int total_bonuses = 0;
+        Continent[] continents = Continent.values();
+        for (int i = 0; i < 6; i++){
+            Continent continent = continents[i];
+            boolean bonus_result = controlsContinent(player, continent, gameState);
+            if (bonus_result) {
+                if (continent == Continent.ASIA) {
+                    total_bonuses += 7;
+                }
+                else if (continent == Continent.EUROPE) {
+                    total_bonuses += 5;
+                }
+                else if (continent == Continent.NORTH_AMERICA) {
+                    total_bonuses += 5;
+                }
+                else if (continent == Continent.SOUTH_AMERICA) {
+                    total_bonuses += 2;
+                }
+                else if (continent == Continent.AUSTRALIA) {
+                    total_bonuses += 2;
+                }
+                else if (continent == Continent.AFRICA) {
+                    total_bonuses += 3;
+                }
+            }
+        }
 
+        return total_bonuses;
     }
     //Optional helpers:
     boolean controlsContinent(Player player, Continent continent, GameState gameState){
-
+        List<Territory> territories = gameState.getTerritories();
+        territories.stream().filter(territory -> territory.getContinent() == continent);
+        List<String> continent_territories = TerritoryCatalog.TERRITORIES_BY_CONTINENT.get(continent);
+        if (territories.size() == continent_territories.size()) {
+            return true;
+        }
+        else{
+            return false;
+        }
     }
-    List<Territory> getTerritoriesInContinent(Continent continent){
-
-    }
-
 }
