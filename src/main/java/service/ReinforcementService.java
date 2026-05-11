a69d6e823e883821f5b1fd08877568880f704e9a
Luke Wejman <lukewejman2027@u.northwestern.edu>
2026-05-10 20:12:57 -0500

    initial skeleton function code + file creation

---
+++ src/main/java/service/ReinforcementService.java
@@ -0,0 +1,20 @@
+package service;
+
+import model.Player;
+import model.GameState;
+import model.Territory;
+
+public class ReinforcementService {
+
+
+    void placeReinforcements(Player player, Territory territory, int armies, GameState gameState){
+
+    }
+    //Optional helpers:
+    boolean canPlaceReinforcements(Player player, Territory territory, int armies, GameState gameState){
+
+    }
+    int getRemainingReinforcements(Player player, GameState gameState){
+
+    }
+}
