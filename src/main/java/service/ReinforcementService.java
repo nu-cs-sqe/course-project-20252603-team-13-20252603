c24911012361be2fd6ce2c47a540cd5a085bd0b1
Luke Wejman <lukewejman2027@u.northwestern.edu>
2026-05-10 20:56:40 -0500

    skeleton code outline, not final, no tests

--- src/main/java/service/ReinforcementService.java
+++ src/main/java/service/ReinforcementService.java
@@ -8,13 +8,25 @@ public class ReinforcementService {
 
 
     void placeReinforcements(Player player, Territory territory, int armies, GameState gameState){
+        boolean result = canPlaceReinforcements(player, territory, armies, gameState);
+        if (result) {
+            
+        }
+        else {
+            throw new IllegalArgumentException("Your army value or territory is not valid, please try again.");
+        }
 
     }
     //Optional helpers:
     boolean canPlaceReinforcements(Player player, Territory territory, int armies, GameState gameState){
 
+        if (player != territory.getOwner()){
+            return false;
+        }
+        if (player.getRemainingArmiesToPlace() < armies) {
+            return false;
+        }
+        return true;
     }
-    int getRemainingReinforcements(Player player, GameState gameState){
 
-    }
 }
