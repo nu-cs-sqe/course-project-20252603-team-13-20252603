bbc3584eb1d1942c5199f9a09881d33d95099d95
Luke Wejman <lukewejman2027@u.northwestern.edu>
2026-05-11 08:12:11 -0500

    BVA file created + skeleton tests

--- src/test/java/service/ReinforcementServiceTest.java
+++ src/test/java/service/ReinforcementServiceTest.java
@@ -1,4 +1,30 @@
 package service;
 
+import org.easymock.EasyMock;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ReinforcementServiceTest {
+
+    @Test
+    void shouldReturnZeroBonusWhenPlayerControlsNoFullContinent() {
+
+    }
+
+    @Test
+    void shouldReturnCorrectBonusWhenPlayerControlsOneContinent() {
+
+    }
+
+    @Test
+    void shouldReturnCombinedBonusWhenPlayerControlsMultipleContinents() {
+        
+    }
+
+    @Test
+    void shouldNotGrantBonusWhenOneTerritoryInContinentIsOwnedByAnotherPlayer() {
+
+    }
+
 }
