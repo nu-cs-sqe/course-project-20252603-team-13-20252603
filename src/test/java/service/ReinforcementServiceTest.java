416c00adacbcd9935928196020b861927e652d7b
Luke Wejman <lukewejman2027@u.northwestern.edu>
2026-05-11 20:58:45 -0500

    Boundary Testing Complete

--- src/test/java/service/ReinforcementServiceTest.java
+++ src/test/java/service/ReinforcementServiceTest.java
@@ -121,20 +121,116 @@ public class ReinforcementServiceTest {
         int actual_owned_terr9 = rs.calculateBaseReinforcements(player9, gameState1);
         assertEquals(6, actual_owned_terr9);
         // 21 -- > 7 armies
+        Territory t21 = new Territory();
+        List<Territory> controlled_territories10 = List.of(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20, t21);
+        Player player10 = new Player(1, "A", "Red", 0, controlled_territories10);
+        int actual_owned_terr10 = rs.calculateBaseReinforcements(player10, gameState1);
+        assertEquals(7, actual_owned_terr10);
         // 23 -- > 7 armies
+        Territory t22 = new Territory();
+        Territory t23 = new Territory();
+        List<Territory> controlled_territories11 = List.of(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20, t21,
+                t22, t23);
+        Player player11 = new Player(1, "A", "Red", 0, controlled_territories11);
+        int actual_owned_terr11 = rs.calculateBaseReinforcements(player11, gameState1);
+        assertEquals(7, actual_owned_terr11);
         // 24 -- > 8 armies
+        Territory t24 = new Territory();
+        List<Territory> controlled_territories12 = List.of(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20, t21,
+                t22, t23, t24);
+        Player player12 = new Player(1, "A", "Red", 0, controlled_territories12);
+        int actual_owned_terr12 = rs.calculateBaseReinforcements(player12, gameState1);
+        assertEquals(8, actual_owned_terr12);
         // 26 -- > 8 armies
+        Territory t25 = new Territory();
+        Territory t26 = new Territory();
+        List<Territory> controlled_territories13 = List.of(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20, t21,
+                t22, t23, t24, t25, t26);
+        Player player13 = new Player(1, "A", "Red", 0, controlled_territories13);
+        int actual_owned_terr13 = rs.calculateBaseReinforcements(player13, gameState1);
+        assertEquals(8, actual_owned_terr13);
         // 27 -- > 9 armies
+        Territory t27 = new Territory();
+        List<Territory> controlled_territories14 = List.of(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20, t21,
+                t22, t23, t24, t25, t26, t27);
+        Player player14 = new Player(1, "A", "Red", 0, controlled_territories14);
+        int actual_owned_terr14 = rs.calculateBaseReinforcements(player14, gameState1);
+        assertEquals(9, actual_owned_terr14);
         // 29 -- > 9 armies
+        Territory t28 = new Territory();
+        Territory t29 = new Territory();
+        List<Territory> controlled_territories15 = List.of(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20, t21,
+                t22, t23, t24, t25, t26, t27, t28, t29);
+        Player player15 = new Player(1, "A", "Red", 0, controlled_territories15);
+        int actual_owned_terr15 = rs.calculateBaseReinforcements(player15, gameState1);
+        assertEquals(9, actual_owned_terr15);
         // 30 -- > 10 armies
+        Territory t30 = new Territory();
+        List<Territory> controlled_territories16 = List.of(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20, t21,
+                t22, t23, t24, t25, t26, t27, t28, t29, t30);
+        Player player16 = new Player(1, "A", "Red", 0, controlled_territories16);
+        int actual_owned_terr16 = rs.calculateBaseReinforcements(player16, gameState1);
+        assertEquals(10, actual_owned_terr16);
         // 32 -- > 10 armies
+        Territory t31 = new Territory();
+        Territory t32 = new Territory();
+        List<Territory> controlled_territories17 = List.of(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20, t21,
+                t22, t23, t24, t25, t26, t27, t28, t29, t30, t31, t32);
+        Player player17 = new Player(1, "A", "Red", 0, controlled_territories17);
+        int actual_owned_terr17 = rs.calculateBaseReinforcements(player17, gameState1);
+        assertEquals(10, actual_owned_terr17);
         // 33 -- > 11 armies
+        Territory t33 = new Territory();
+        List<Territory> controlled_territories18 = List.of(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20, t21,
+                t22, t23, t24, t25, t26, t27, t28, t29, t30, t31, t32, t33);
+        Player player18 = new Player(1, "A", "Red", 0, controlled_territories18);
+        int actual_owned_terr18 = rs.calculateBaseReinforcements(player18, gameState1);
+        assertEquals(11, actual_owned_terr18);
         // 35 -- > 11 armies
+        Territory t34 = new Territory();
+        Territory t35 = new Territory();
+        List<Territory> controlled_territories19 = List.of(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20, t21,
+                t22, t23, t24, t25, t26, t27, t28, t29, t30, t31, t32, t33, t34, t35);
+        Player player19 = new Player(1, "A", "Red", 0, controlled_territories19);
+        int actual_owned_terr19 = rs.calculateBaseReinforcements(player19, gameState1);
+        assertEquals(11, actual_owned_terr19);
         // 36 -- > 12 armies
+        Territory t36 = new Territory();
+        List<Territory> controlled_territories20 = List.of(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20, t21,
+                t22, t23, t24, t25, t26, t27, t28, t29, t30, t31, t32, t33, t34, t35, t36);
+        Player player20 = new Player(1, "A", "Red", 0, controlled_territories20);
+        int actual_owned_terr20 = rs.calculateBaseReinforcements(player20, gameState1);
+        assertEquals(12, actual_owned_terr20);
         // 38 -- > 12 armies
+        Territory t37 = new Territory();
+        Territory t38 = new Territory();
+        List<Territory> controlled_territories21 = List.of(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20, t21,
+                t22, t23, t24, t25, t26, t27, t28, t29, t30, t31, t32, t33, t34, t35, t36, t37, t38);
+        Player player21 = new Player(1, "A", "Red", 0, controlled_territories21);
+        int actual_owned_terr21 = rs.calculateBaseReinforcements(player21, gameState1);
+        assertEquals(12, actual_owned_terr21);
         // 39 -- > 13 armies
+        Territory t39 = new Territory();
+        List<Territory> controlled_territories22 = List.of(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20, t21,
+                t22, t23, t24, t25, t26, t27, t28, t29, t30, t31, t32, t33, t34, t35, t36, t37, t38, t39);
+        Player player22 = new Player(1, "A", "Red", 0, controlled_territories22);
+        int actual_owned_terr22 = rs.calculateBaseReinforcements(player22, gameState1);
+        assertEquals(13, actual_owned_terr22);
         // 41 -- > 13 armies
+        Territory t40 = new Territory();
+        Territory t41 = new Territory();
+        List<Territory> controlled_territories23 = List.of(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20, t21,
+                t22, t23, t24, t25, t26, t27, t28, t29, t30, t31, t32, t33, t34, t35, t36, t37, t38, t39, t40, t41);
+        Player player23 = new Player(1, "A", "Red", 0, controlled_territories23);
+        int actual_owned_terr23 = rs.calculateBaseReinforcements(player23, gameState1);
+        assertEquals(13, actual_owned_terr23);
         // 42 -- > 14 armies
+        Territory t42 = new Territory();
+        List<Territory> controlled_territories24 = List.of(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20, t21,
+                t22, t23, t24, t25, t26, t27, t28, t29, t30, t31, t32, t33, t34, t35, t36, t37, t38, t39, t40, t41, t42);
+        Player player24 = new Player(1, "A", "Red", 0, controlled_territories24);
+        int actual_owned_terr24 = rs.calculateBaseReinforcements(player24, gameState1);
+        assertEquals(14, actual_owned_terr24);
     }
 
 }
