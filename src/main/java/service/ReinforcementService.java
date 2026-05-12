b6dfcf2a8a107c2ee0994443bf743634e49670de
Luke Wejman <lukewejman2027@u.northwestern.edu>
2026-05-12 17:24:28 -0500

    shouldAllowPlayerToPlaceReinforcementsOnOwnedTerritory completed

--- src/main/java/service/ReinforcementService.java
+++ src/main/java/service/ReinforcementService.java
@@ -11,20 +11,17 @@ public class ReinforcementService {
 
     void placeReinforcements(Player player, Territory territory, int armies, GameState gameState){
         boolean result = canPlaceReinforcements(player, territory, armies, gameState);
+        System.out.println("result:" + result);
         if (result) {
-            List<Player> players = gameState.getPlayers();
-            int player_index = players.indexOf(player);
-            Player pl_found = gameState.getPlayers().get(player_index);
-            List<Territory> territories = gameState.getTerritories();
+            List<Territory> territories = player.getControlledTerritories();
             int territory_index = territories.indexOf(territory);
-            Territory tr_found = gameState.getTerritories().get(territory_index);
-
-            int current_armies_unplaced = pl_found.getRemainingArmiesToPlace();
+            Territory tr_found = territories.get(territory_index);
+            int current_armies_unplaced = player.getRemainingArmiesToPlace();
             int new_remaining_armies = current_armies_unplaced - armies;
             int current_territories_placed = tr_found.getArmyCount();
             int new_territory_army = armies + current_territories_placed;
             tr_found.setArmyCount(new_territory_army);
-            pl_found.setRemainingArmiesToPlace(new_remaining_armies);
+            player.setRemainingArmiesToPlace(new_remaining_armies);
         }
         else {
             throw new IllegalArgumentException("Your army value or territory is not valid, please try again.");
@@ -33,17 +30,16 @@ public class ReinforcementService {
     }
     //Optional helpers:
     boolean canPlaceReinforcements(Player player, Territory territory, int armies, GameState gameState){
-        List<Player> players = gameState.getPlayers();
-        int player_index = players.indexOf(player);
-        Player pl_found = gameState.getPlayers().get(player_index);
-        List<Territory> territories = gameState.getTerritories();
-        int territory_index = territories.indexOf(territory);
-        Territory tr_found = gameState.getTerritories().get(territory_index);
 
-        if (pl_found != tr_found.getOwner()){
+        List<Territory> territories = player.getControlledTerritories();
+        int territory_index = territories.indexOf(territory);
+
+
+        if (territory_index == -1){
             return false;
         }
-        if (pl_found.getRemainingArmiesToPlace() < armies) {
+        // int armies = tr_found.getArmyCount();
+        if (player.getRemainingArmiesToPlace() < armies) {
             return false;
         }
         return true;
--- src/test/java/service/ReinforcementServiceTest.java
+++ src/test/java/service/ReinforcementServiceTest.java
@@ -1,10 +1,6 @@
 package service;
 
-import model.Player;
-import model.Territory;
-import model.GameState;
-import model.GamePhase;
-
+import model.*;
 
 
 import java.util.List;
@@ -18,7 +14,24 @@ public class ReinforcementServiceTest {
 
     @Test
     void shouldAllowPlayerToPlaceReinforcementsOnOwnedTerritory() {
-
+        ReinforcementService rs = new ReinforcementService();
+        GameState gameState = new GameState();
+        Territory t1 = new Territory();
+        Territory t2 = new Territory();
+        List<Territory> controlled_territories1 = List.of(t1, t2);
+        Player player1 = new Player(1, "A", "Red", 4, controlled_territories1);
+        t1.setOwner(player1);
+        t1.setOwner(player1);
+        t1.setArmyCount(3);
+        t2.setArmyCount(1);
+        t1.setContinent(Continent.AUSTRALIA);
+        t2.setContinent(Continent.NORTH_AMERICA);
+        t1.setName("Indonesia");
+        t2.setName("Greenland");
+        int armies_we_will_place = 4;
+        rs.placeReinforcements(player1, t1, armies_we_will_place, gameState);
+        int new_army_count_post_place = t1.getArmyCount();
+        assertEquals(7, new_army_count_post_place);
     }
 
     @Test
