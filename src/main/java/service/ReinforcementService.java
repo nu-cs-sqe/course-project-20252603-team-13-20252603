ab485feec3ec7860c804ee923dd1dcd835fb1814
Luke Wejman <lukewejman2027@u.northwestern.edu>
2026-05-13 12:13:23 -0500

    Comments added

--- src/main/java/service/ReinforcementService.java
+++ src/main/java/service/ReinforcementService.java
@@ -12,12 +12,15 @@ import java.util.stream.Collectors;
 
 public class ReinforcementService {
 
-
+    /**
+     * Calculates Continent Bonus: accumulates bonus for every continent a player controls entirety
+     */
     int calculateContinentBonus(Player player, GameState gameState){
         int total_bonuses = 0;
         Continent[] continents = Continent.values();
         for (int i = 0; i < 6; i++){
             Continent continent = continents[i];
+            // determine if the player controls every territory per continent
             boolean bonus_result = controlsContinent(player, continent, gameState);
             if (bonus_result) {
                 if (continent == Continent.ASIA) {
@@ -43,10 +46,16 @@ public class ReinforcementService {
 
         return total_bonuses;
     }
-    //Optional helpers:
+
+    /**
+     * Determines if a player controls territories of an entire continent
+     */
     boolean controlsContinent(Player player, Continent continent, GameState gameState){
+        // get all controlled territories
         List<Territory> territories = player.getControlledTerritories();
+        // filter controlled territories by the input continent, and convert stream to a list
         List<Territory> new_territories = territories.stream().filter(territory -> territory.getContinent() == continent).collect(Collectors.toList());
+        // get all territories in the provided continent
         List<String> continent_territories = TerritoryCatalog.TERRITORIES_BY_CONTINENT.get(continent);
         if (new_territories.size() == continent_territories.size()) {
             return true;
