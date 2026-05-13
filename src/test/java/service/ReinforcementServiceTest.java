3c02e1851f0896246ecccbd3f66b35262ca2cd87
Luke Wejman <lukewejman2027@u.northwestern.edu>
2026-05-13 06:56:46 -0500

    shouldUpdateTerritoryArmyCountAfterValidPlacement completed

--- src/test/java/service/ReinforcementServiceTest.java
+++ src/test/java/service/ReinforcementServiceTest.java
@@ -118,7 +118,24 @@ public class ReinforcementServiceTest {
 
     @Test
     void shouldUpdateTerritoryArmyCountAfterValidPlacement() {
-
+        ReinforcementService rs = new ReinforcementService();
+        GameState gameState = new GameState();
+        Territory t1 = new Territory();
+        Territory t2 = new Territory();
+        List<Territory> controlled_territories1 = List.of(t1, t2);
+        Player player1 = new Player(1, "A", "Red", 4, controlled_territories1);
+        t1.setOwner(player1);
+        t2.setOwner(player1);
+        t1.setArmyCount(3);
+        t2.setArmyCount(1);
+        t1.setContinent(Continent.AUSTRALIA);
+        t2.setContinent(Continent.NORTH_AMERICA);
+        t1.setName("Indonesia");
+        t2.setName("Greenland");
+        int armies_we_will_place = 2;
+        rs.placeReinforcements(player1, t1, armies_we_will_place, gameState);
+        int new_army_count_post_place = t1.getArmyCount();
+        assertEquals(5, new_army_count_post_place);
     }
 
 }
