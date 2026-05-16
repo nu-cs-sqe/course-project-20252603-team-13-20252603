ef853bf9c25e8d3d22f762afab25446e1a887926
Luke Wejman <lukewejman2027@u.northwestern.edu>
2026-05-15 20:42:40 -0500

    minor bug fixed

--- src/test/java/service/ReinforcementServiceTest.java
+++ src/test/java/service/ReinforcementServiceTest.java
@@ -197,8 +197,8 @@ public class ReinforcementServiceTest {
         int actual_owned_terr14 = rs.calculateBaseReinforcements(player1, gameState1);
         assertEquals(9, actual_owned_terr14);
         // TC15: 29 -- > 9 armies
-        controlled_territories.add(t29);
-        controlled_territories.add(t30);
+        controlled_territories.add(t28);
+        controlled_territories.add(t39);
         player1.setControlledTerritories(controlled_territories);
         int actual_owned_terr15 = rs.calculateBaseReinforcements(player1, gameState1);
         assertEquals(9, actual_owned_terr15);
