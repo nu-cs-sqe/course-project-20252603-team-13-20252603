480cd52e0676c626ae3f2b0d26d3ddbe69681e98
Luke Wejman <lukewejman2027@u.northwestern.edu>
2026-05-15 20:28:09 -0500

    removed unneeded files + simplified tests

--- src/test/java/service/ReinforcementServiceTest.java
+++ src/test/java/service/ReinforcementServiceTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 import net.bytebuddy.pool.TypePool;
 import org.easymock.EasyMock;
 import org.junit.jupiter.api.Test;
-
+import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ReinforcementServiceTest {
@@ -17,18 +17,13 @@ public class ReinforcementServiceTest {
     void shouldAllowPlayerToPlaceReinforcementsOnOwnedTerritory() {
         ReinforcementService rs = new ReinforcementService();
         GameState gameState = new GameState();
-        Territory t1 = new Territory();
-        Territory t2 = new Territory();
-        List<Territory> controlled_territories1 = List.of(t1, t2);
+        List<Territory> controlled_territories1 = new ArrayList<>();
         Player player1 = new Player(1, "A", "Red", 4, controlled_territories1);
-        t1.setOwner(player1);
-        t2.setOwner(player1);
-        t1.setArmyCount(3);
-        t2.setArmyCount(1);
-        t1.setContinent(Continent.AUSTRALIA);
-        t2.setContinent(Continent.NORTH_AMERICA);
-        t1.setName("Indonesia");
-        t2.setName("Greenland");
+        Territory t1 = new Territory("Indonesia", player1, 3, Continent.AUSTRALIA);
+        Territory t2 = new Territory("Greenland", player1, 1, Continent.NORTH_AMERICA);
+        controlled_territories1.add(t1);
+        controlled_territories1.add(t2);
+        player1.setControlledTerritories(controlled_territories1);
         int armies_we_will_place = 4;
         assertDoesNotThrow(() -> rs.placeReinforcements(player1, t1, armies_we_will_place, gameState));
     }
@@ -37,25 +32,18 @@ public class ReinforcementServiceTest {
     void shouldRejectPlacementOnTerritoryNotOwnedByPlayer() {
         ReinforcementService rs = new ReinforcementService();
         GameState gameState = new GameState();
-        Territory t1 = new Territory();
-        Territory t2 = new Territory();
-        Territory t3 = new Territory();
-        List<Territory> controlled_territories1 = List.of(t1, t2);
-        List<Territory> controlled_territories2= List.of(t3);
+        List<Territory> controlled_territories1 = new ArrayList<>();
+        List<Territory> controlled_territories2 = new ArrayList<>();
         Player player1 = new Player(1, "A", "Red", 4, controlled_territories1);
         Player player2 = new Player(2, "B", "Blue", 4, controlled_territories2);
-        t1.setOwner(player1);
-        t2.setOwner(player1);
-        t3.setOwner(player2);
-        t1.setArmyCount(3);
-        t2.setArmyCount(1);
-        t3.setArmyCount(1);
-        t1.setContinent(Continent.AUSTRALIA);
-        t2.setContinent(Continent.NORTH_AMERICA);
-        t3.setContinent(Continent.SOUTH_AMERICA);
-        t1.setName("Indonesia");
-        t2.setName("Greenland");
-        t3.setName("Peru");
+        Territory t1 = new Territory("Indonesia", player1, 3, Continent.AUSTRALIA);
+        Territory t2 = new Territory("Greenland", player1, 1, Continent.NORTH_AMERICA);
+        Territory t3 = new Territory("Peru", player2, 1, Continent.SOUTH_AMERICA);
+        controlled_territories1.add(t1);
+        controlled_territories1.add(t2);
+        controlled_territories2.add(t3);
+        player1.setControlledTerritories(controlled_territories1);
+        player2.setControlledTerritories(controlled_territories2);
         int armies_we_will_place = 4;
         assertThrows(IllegalArgumentException.class, () -> {
             rs.placeReinforcements(player1, t3, armies_we_will_place, gameState);
@@ -66,25 +54,13 @@ public class ReinforcementServiceTest {
     void shouldRejectPlacementWhenArmiesExceedRemainingReinforcements() {
         ReinforcementService rs = new ReinforcementService();
         GameState gameState = new GameState();
-        Territory t1 = new Territory();
-        Territory t2 = new Territory();
-        Territory t3 = new Territory();
-        List<Territory> controlled_territories1 = List.of(t1, t2);
-        List<Territory> controlled_territories2= List.of(t3);
+        List<Territory> controlled_territories1 = new ArrayList<>();
         Player player1 = new Player(1, "A", "Red", 4, controlled_territories1);
-        Player player2 = new Player(2, "B", "Blue", 4, controlled_territories2);
-        t1.setOwner(player1);
-        t2.setOwner(player1);
-        t3.setOwner(player2);
-        t1.setArmyCount(3);
-        t2.setArmyCount(1);
-        t3.setArmyCount(1);
-        t1.setContinent(Continent.AUSTRALIA);
-        t2.setContinent(Continent.NORTH_AMERICA);
-        t3.setContinent(Continent.SOUTH_AMERICA);
-        t1.setName("Indonesia");
-        t2.setName("Greenland");
-        t3.setName("Peru");
+        Territory t1 = new Territory("Indonesia", player1, 3, Continent.AUSTRALIA);
+        Territory t2 = new Territory("Greenland", player1, 1, Continent.NORTH_AMERICA);
+        controlled_territories1.add(t1);
+        controlled_territories1.add(t2);
+        player1.setControlledTerritories(controlled_territories1);
         int armies_we_will_place = 5;
         assertThrows(IllegalArgumentException.class, () -> {
             rs.placeReinforcements(player1, t2, armies_we_will_place, gameState);
@@ -95,18 +71,13 @@ public class ReinforcementServiceTest {
     void shouldDecreaseRemainingReinforcementsAfterValidPlacement() {
         ReinforcementService rs = new ReinforcementService();
         GameState gameState = new GameState();
-        Territory t1 = new Territory();
-        Territory t2 = new Territory();
-        List<Territory> controlled_territories1 = List.of(t1, t2);
+        List<Territory> controlled_territories1 = new ArrayList<>();
         Player player1 = new Player(1, "A", "Red", 4, controlled_territories1);
-        t1.setOwner(player1);
-        t2.setOwner(player1);
-        t1.setArmyCount(3);
-        t2.setArmyCount(1);
-        t1.setContinent(Continent.AUSTRALIA);
-        t2.setContinent(Continent.NORTH_AMERICA);
-        t1.setName("Indonesia");
-        t2.setName("Greenland");
+        Territory t1 = new Territory("Indonesia", player1, 3, Continent.AUSTRALIA);
+        Territory t2 = new Territory("Greenland", player1, 1, Continent.NORTH_AMERICA);
+        controlled_territories1.add(t1);
+        controlled_territories1.add(t2);
+        player1.setControlledTerritories(controlled_territories1);
         int armies_we_will_place = 3;
         rs.placeReinforcements(player1, t1, armies_we_will_place, gameState);
         int remaining_army_count_post_place = player1.getRemainingArmiesToPlace();
@@ -118,18 +89,13 @@ public class ReinforcementServiceTest {
     void shouldUpdateTerritoryArmyCountAfterValidPlacement() {
         ReinforcementService rs = new ReinforcementService();
         GameState gameState = new GameState();
-        Territory t1 = new Territory();
-        Territory t2 = new Territory();
-        List<Territory> controlled_territories1 = List.of(t1, t2);
+        List<Territory> controlled_territories1 = new ArrayList<>();
         Player player1 = new Player(1, "A", "Red", 4, controlled_territories1);
-        t1.setOwner(player1);
-        t2.setOwner(player1);
-        t1.setArmyCount(3);
-        t2.setArmyCount(1);
-        t1.setContinent(Continent.AUSTRALIA);
-        t2.setContinent(Continent.NORTH_AMERICA);
-        t1.setName("Indonesia");
-        t2.setName("Greenland");
+        Territory t1 = new Territory("Indonesia", player1, 3, Continent.AUSTRALIA);
+        Territory t2 = new Territory("Greenland", player1, 1, Continent.NORTH_AMERICA);
+        controlled_territories1.add(t1);
+        controlled_territories1.add(t2);
+        player1.setControlledTerritories(controlled_territories1);
         int armies_we_will_place = 2;
         rs.placeReinforcements(player1, t1, armies_we_will_place, gameState);
         int new_army_count_post_place = t1.getArmyCount();
